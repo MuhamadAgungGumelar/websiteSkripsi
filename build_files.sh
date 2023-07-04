0d45d8f69bd71d75ea5eeec32b1f6fb9026e685c
MuhamadAgungGumelar <mohamadagung007@gmail.com>
2023-07-04 15:32:09 +0700

    menambahkan file untuk deploy ke vercel

---
+++ build_files.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+python3.7.9 manage.py collecstatic
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -148,12 +148,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'database/images/')
